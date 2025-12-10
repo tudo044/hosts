229362305e992ca0d704c94623a0a31d26bb1735
tudo044 <andreitudorache68@gmail.com>
2025-12-10 10:33:05 +0000

    Adaugare functie validare DNS

--- verificare.sh
+++ verificare.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+valideaza() {
+if host $1 $3 | grep -q $2; then
+echo "Valid: $1 -> $2"
+else
+echo "Invalid: $1 -> $2"
+fi
+}
 while read line; do
 [[ "$line" =~ ^#.*$ ]] && continue
 [[ -z "$line" ]] && continue
 ip=$(echo $line | awk '{print $1}')
 name=$(echo $line | awk '{print $2}')
-echo "IP: $ip Host: $name"
+valideaza $name $ip 8.8.8.8
 done < /etc/hosts
